ec47bde9ae4411d857be56c3744765777e16332e
bgh331 <58461569+bgh331@users.noreply.github.com>
2020-04-03 12:19:11 +0300

    Add files via upload

--- ProjektiAPI/ProjektiAPI/src/main/java/ProjektiAPI/ProjektiAPI/utils/PopulateDB.java
+++ ProjektiAPI/ProjektiAPI/src/main/java/ProjektiAPI/ProjektiAPI/utils/PopulateDB.java
@@ -15,7 +15,10 @@ public class PopulateDB {
 	public void populate() {
 		repository.deleteAll();
 		
-		repository.save(new Kysely("Minkä kokoinen on Islanti?", "a) Iso", "b) Keskikokoinen", "c) Pieni"));
+		repository.save(new Kysely("Ajan ja tehtävirn hallinta", "Teen tehtäväni sitä mukaan kun ne ilmestyy.", "Teen töistä aikataulun, jota pyrin noudattamaan.", 
+				"Suunnittelen ja priorisoin säännölliset työtehtävät keskittyen olennaiseen."));
+		repository.save(new Kysely("Stressinhallinta", "Tiedän kuinka stressiä voisi hallita, mutta tunnen itseni usein stressaantuneeksi.", "Saan usein hallittua omat stressitasoni.", 
+				"Stressinhallintani voisi olla parempi työympäristössä."));
 		
 	}
 }
